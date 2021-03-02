f181c94cf33c3f04fd243062087a1ae260a15451
lantip <lantip@gmail.com>
2021-03-02 17:54:51 +0700

    02 03 2021

--- ocr_bnpb.py
+++ ocr_bnpb.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageEnhance
 from pytesseract import image_to_string
 import json
 from os import walk
@@ -18,7 +18,7 @@ def ocr_date(jpg):
     for lin in lins:
         if len(lin.strip()) > 1:
             lines.append(lin)    
-
+    
     if ' | ' in lines[11]:
         lne = lines[10].split()
         tahap = []
@@ -37,18 +37,61 @@ def ocr_date(jpg):
         result['tahap_2'] = ssr[3].replace('(','').replace(')','').replace('+','').replace('.','').replace(' ','')
     else:
         ssr = lines[10].split('|')
-        result['total_sasaran'] = ssr[1].replace('.','').replace(' ','')
-        result['sasaran_sdmk'] = ssr[0].replace('.','').replace(' ','')
-        lne = lines[9].split()
-        tahap = []
-        for thp in lne:
-            cdgt = re.findall(r'\d+', thp)
-            if len(cdgt) > 0:
-                tahap.append(cdgt)
-        for idx, val in enumerate(tahap):
-            thps = ''.join(val)
-            result['penambahan_tahap_'+str(idx+1)] = thps.replace(' ','')
-        result['tahap_1'] = ssr[2].replace('(','').replace(')','').replace('+','').replace('.','').replace(' ','')
-        result['tahap_2'] = ssr[3].replace('(','').replace(')','').replace('+','').replace('.','').replace(' ','')
+        if len(ssr) < 4:
+            enhancer = ImageEnhance.Contrast(im_invert)
+            im_output = enhancer.enhance(5)
+            img = im_output.convert('LA')
+            data = image_to_string(img)
+            lins = data.split('\n')
+            lines = []
+            for lin in lins:
+                if len(lin.strip()) > 1:
+                    lines.append(lin)  
+            ssre = lines[9].split('|') # sementara
+            ssr = []
+            for sse in ssre:
+                tte = sse.strip()
+                if len(tte.split()) > 1:
+                    for tt in tte.split():
+                        ssr.append(tt.strip())
+                else:
+                    ssr.append(tte)
+            result['total_sasaran'] = ssr[1].replace('.','').replace(' ','')
+            result['sasaran_sdmk'] = ssr[0].replace('.','').replace(' ','')
+            lne = lines[8].split()
+            tahap = []
+            for thp in lne:
+                cdgt = re.findall(r'\d+', thp)
+                if len(cdgt) > 0:
+                    tahap.append(cdgt)
+            for idx, val in enumerate(tahap):
+                thps = ''.join(val)
+                if idx > 0:
+                    before = ''.join(tahap[idx-1])
+                    if int(thps.replace(' ', '')) > int(before.replace(' ','')):
+                        result['penambahan_tahap_'+str(idx+1)] = thps.replace(' ','')[1:]
+                    else:
+                        result['penambahan_tahap_'+str(idx+1)] = thps.replace(' ','')
+                else:        
+                    result['penambahan_tahap_'+str(idx+1)] = thps.replace(' ','')
+            tahap1 = ssr[2].replace('(','').replace(')','').replace('+','').replace('.','').replace(' ','')
+            tahap2 = ssr[3].replace('(','').replace(')','').replace('+','').replace('.','').replace(' ','')
+            result['tahap_1'] = tahap1
+            result['tahap_2'] = tahap2
+
+        else:
+            result['total_sasaran'] = ssr[1].replace('.','').replace(' ','')
+            result['sasaran_sdmk'] = ssr[0].replace('.','').replace(' ','')
+            lne = lines[9].split()
+            tahap = []
+            for thp in lne:
+                cdgt = re.findall(r'\d+', thp)
+                if len(cdgt) > 0:
+                    tahap.append(cdgt)
+            for idx, val in enumerate(tahap):
+                thps = ''.join(val)
+                result['penambahan_tahap_'+str(idx+1)] = thps.replace(' ','')
+            result['tahap_1'] = ssr[2].replace('(','').replace(')','').replace('+','').replace('.','').replace(' ','')
+            result['tahap_2'] = ssr[3].replace('(','').replace(')','').replace('+','').replace('.','').replace(' ','')
 
     return result
